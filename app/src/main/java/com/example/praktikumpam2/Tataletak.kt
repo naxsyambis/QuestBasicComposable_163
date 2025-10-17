be5ffb231b9ec9106199f7c9af92f88bf30b6e01
naxsyambis <naxsyambis@gmail.com>
2025-10-17 15:51:39 +0700

    Menambahkan isi tata letak column row

--- app/src/main/java/com/example/praktikumpam2/Tataletak.kt
+++ app/src/main/java/com/example/praktikumpam2/Tataletak.kt
@@ -61,5 +61,21 @@ fun TataletakBox(modifier: Modifier) {
 
 @Composable
 fun TataletakColumnRow(modifier: Modifier) {
-
+    Column() {
+        //Baris 1
+        Row(modifier = modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly) {
+            Text(text = "Komponen1Baris1")
+            Text(text = "Komponen2Baris1")
+            Text(text = "Komponen3Baris1")
+        }
+        //Baris2
+        Row(modifier = modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly) {
+            Text(text = "Komponen1Baris2")
+            Text(text = "Komponen2Baris2")
+            Text(text = "Komponen3Baris2")
+        }
+    }
 }
+
