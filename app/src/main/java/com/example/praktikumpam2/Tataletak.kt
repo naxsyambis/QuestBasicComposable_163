456d86d84fd1e279d66df732540413ad6d3a65dd
naxsyambis <naxsyambis@gmail.com>
2025-10-17 15:58:47 +0700

    menambahkan code di composable row column

--- app/src/main/java/com/example/praktikumpam2/Tataletak.kt
+++ app/src/main/java/com/example/praktikumpam2/Tataletak.kt
@@ -81,5 +81,18 @@ fun TataletakColumnRow(modifier: Modifier) {
 
 @Composable
 fun TataletakRowColumn(modifier: Modifier) {
-
+    Row(modifier = modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
+        //Kolom1
+        Column() {
+            Text(text = "Komponen1Kolom1")
+            Text(text = "Komponen2Kolom1")
+            Text(text = "Komponen3Kolom1")
+        }
+        //Kolom 2
+        Column() {
+            Text(text = "Komponen1Kolom2")
+            Text(text = "Komponen2Kolom2")
+            Text(text = "Komponen3Kolom2")
+        }
+    }
 }
