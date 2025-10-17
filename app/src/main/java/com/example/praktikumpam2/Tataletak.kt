bb0f2e5426c328ae7beda197feeb714bbc3788af
naxsyambis <naxsyambis@gmail.com>
2025-10-17 16:21:35 +0700

    menambahkan modifier spacer,

--- app/src/main/java/com/example/praktikumpam2/Tataletak.kt
+++ app/src/main/java/com/example/praktikumpam2/Tataletak.kt
@@ -127,6 +127,25 @@ fun TataletakBoxColumnRow(modifier: Modifier) {
                 }
             }
         }
-
+        Spacer(modifier = Modifier.height(height = 10.dp))
+        Box(
+            modifier = modifier
+                .fillMaxWidth()
+                .height(height = 300.dp)
+                .background(color = Color.Cyan),
+            contentAlignment = Alignment.Center
+        ){
+            Image(painter = gambar,
+                contentDescription = null,
+                contentScale = ContentScale.Fit)
+            Text(text = "My Music",
+                fontSize = 50.sp,
+                color = Color.Red,
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily.Cursive,
+                modifier = Modifier.align(
+                    alignment = Alignment.Center
+                ))
+        }
     }
 }
